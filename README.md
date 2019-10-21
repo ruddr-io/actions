00f71ceecb6456948e10c3fa64ff5a6e6bb7c627
Mason Hugus <mason@nexza.com>
2019-10-21 16:08:04 -0400

    Add ref-to-slug action

---
+++ README.md
@@ -0,0 +1,7 @@
+# Github Actions
+
+A collection of useful GitHub Actions.
+
+## [GitHub Reference to Slug](ref-to-slug)
+
+Convert the `GITHUB_REF` to a simple slug that can be used in URIs, filenames, and IDs.
---
+++ ref-to-slug/index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+const slugify = require('@sindresorhus/slugify');
+
+async function run() {
+  try {
+    const ref = process.env.GITHUB_REF;
+    if (!ref) {
+      throw new Error('No GITHUB_REF environment variable available.');
+    }
+
+    console.log(`Converting '${ref}' to a slug...`);
+
+    const regex = /refs\/(tags|heads)\/(?<name>.*)/i;
+    const refMatch = ref.match(regex);
+    if (!refMatch) {
+      throw new Error('Unrecognized GITHUB_REF format.');
+    }
+
+    const refName = refMatch.groups.name;
+    if (!refName) {
+      throw new Error('No branch or tag name available.');
+    }
+
+    const slug = slugify(refName);
+
+    core.setOutput('slug', slug);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
